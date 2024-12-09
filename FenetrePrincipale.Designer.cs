69eb7b1ec297eae1eb1cadfea9f8789d04f776ff
patdev-qc-ca <patdev.qc.ca@outlook.com>
2024-12-08 20:59:09 -0500

    20241129 15:30
    .NET 9

---
+++ FenetrePrincipale.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace FindItemInfo_net9
+{
+    partial class FenetrePrincipale
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FenetrePrincipale));
+            label1 = new Label();
+            textBox1 = new TextBox();
+            listView1 = new ListView();
+            button2 = new Button();
+            button1 = new Button();
+            linkLabel1 = new LinkLabel();
+            linkLabel2 = new LinkLabel();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(15, 15);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(140, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Descriptif de la recherche";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(172, 12);
+            textBox1.Margin = new Padding(4, 3, 4, 3);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(591, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            listView1.CheckBoxes = true;
+            listView1.Location = new Point(19, 45);
+            listView1.Margin = new Padding(4, 3, 4, 3);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(1482, 753);
+            listView1.TabIndex = 2;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged;
+            // 
+            // button2
+            // 
+            button2.AutoSize = true;
+            button2.ImageAlign = ContentAlignment.MiddleLeft;
+            button2.Location = new Point(988, 9);
+            button2.Margin = new Padding(4, 3, 4, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(115, 27);
+            button2.TabIndex = 4;
+            button2.Text = "Exporter vers excel";
+            button2.TextAlign = ContentAlignment.MiddleRight;
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.AutoSize = true;
+            button1.BackgroundImageLayout = ImageLayout.None;
+            button1.ImageAlign = ContentAlignment.MiddleLeft;
+            button1.Location = new Point(904, 9);
+            button1.Margin = new Padding(4, 3, 4, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(76, 27);
+            button1.TabIndex = 3;
+            button1.Text = "Rechercher";
+            button1.TextAlign = ContentAlignment.MiddleRight;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(1130, 802);
+            linkLabel1.Margin = new Padding(4, 0, 4, 0);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(166, 15);
+            linkLabel1.TabIndex = 5;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Sélectionner tous les éléments";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.AutoSize = true;
+            linkLabel2.Location = new Point(1312, 802);
+            linkLabel2.Margin = new Padding(4, 0, 4, 0);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.Size = new Size(179, 15);
+            linkLabel2.TabIndex = 6;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "Désélectionner tous les éléments";
+            linkLabel2.LinkClicked += linkLabel2_LinkClicked;
+            // 
+            // button3
+            // 
+            button3.AutoSize = true;
+            button3.Location = new Point(770, 9);
+            button3.Margin = new Padding(4, 3, 4, 3);
+            button3.Name = "button3";
+            button3.Size = new Size(126, 27);
+            button3.TabIndex = 7;
+            button3.Text = "Coller et rechercher";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // FenetrePrincipale
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1527, 827);
+            Controls.Add(button3);
+            Controls.Add(linkLabel2);
+            Controls.Add(linkLabel1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(listView1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(4, 3, 4, 3);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "FenetrePrincipale";
+            Text = "Robot de recherche semantique par description de pieces";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ FenetrePrincipale.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Office.Interop.Excel;
+
+namespace FindItemInfo_net9
+{
+    public partial class FenetrePrincipale : Form
+    {
+        public FenetrePrincipale()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] requete = textBox1.Text.Split(' ');
+            string sql = "SELECT [IDProjet],[NumItem],[Desc_FR],[Desc_EN],Manufact ,NomFournisseur FROM [autogrb].[dbo].[Projet_Pieces] " +
+                "join Fournisseur on Projet_Pieces.IDFRS=Fournisseur.IDFRS " +
+                "where ";
+            for (int x = 0; x < requete.Length; x++)
+            {
+                if (x == requete.Length - 1)
+                {
+                    sql += "Desc_FR like '%ble%'";
+
+                }
+                else
+                {
+                    sql += "Desc_FR like '%ble%' or ";
+                }
+            }
+            sql += " order by Desc_FR asc";
+            SqlConnection con = new SqlConnection("Data Source=PRODUCTION\\SQLEXPRESS;Initial Catalog=AutoGRB;Integrated Security=false;Persist Security Info=True;User ID=sa; pwd=$NK#Bpl9YCm!0EKeZLdzp$Qetrz9g9bdQK7LO8L!u4oyv4rO2AOEvceyu8XIo;");
+            con.Open();
+            SqlDataReader rst = new SqlCommand(sql, con).ExecuteReader();
+            while (rst.Read())
+            {
+                ListViewItem itm = listView1.Items.Add(string.Empty);
+                itm.SubItems.Insert(0, new ListViewItem.ListViewSubItem(null, rst[0].ToString()));
+                itm.SubItems.Insert(1, new ListViewItem.ListViewSubItem(null, rst[1].ToString()));
+                itm.SubItems.Insert(2, new ListViewItem.ListViewSubItem(null, rst[2].ToString()));
+                itm.SubItems.Insert(3, new ListViewItem.ListViewSubItem(null, rst[3].ToString()));
+                itm.SubItems.Insert(4, new ListViewItem.ListViewSubItem(null, rst[4].ToString()));
+                itm.SubItems.Insert(5, new ListViewItem.ListViewSubItem(null, rst[5].ToString()));
+            }
+            rst.Close();
+            rst = null;
+            con.Close();
+            if (listView1.Items.Count <1)
+            {
+                MessageBox.Show("Données insuffisantes pour formuler une conclusion");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] colones = {  "IDProjet","NumItem","Desc_FR","Desc_EN","Manufact","NomFournisseur"};
+            foreach (string p in colones) {
+                listView1.Columns.Add(p);
+            }
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListViewItem item = listView1.FocusedItem;
+            MessageBox.Show($"IDProjet:{item.SubItems[0].Text}\nNumItem:{item.SubItems[1].Text}\nDesc_FR:{item.SubItems[2].Text}\nDesc_EN:{item.SubItems[3].Text}" +
+                $"\nManufact:{item.SubItems[4].Text}\nNomFournisseur:{item.SubItems[5].Text}\n", this.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Excel.Application oXLApp = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbook oXLBook = oXLApp.Workbooks.Add(string.Empty);
+            Microsoft.Office.Interop.Excel.Worksheet oXLSheet = (Worksheet)oXLBook.Worksheets[1];
+            oXLApp.Visible = true;
+            oXLSheet.Range["A1: F1"].Font.Bold = true;
+            oXLSheet.Name = "Recherche de pieces inventaire";
+            oXLSheet.Range["A:F"].HorizontalAlignment = XlHAlign.xlHAlignCenter;
+            oXLSheet.Range["A1: F1"].Value = new object[] { "IDProjet", "NumItem", "Desc_FR", "Desc_EN", "Manufact", "NomFournisseur" };
+            for (int X = 0; X < listView1.Items.Count; X++)
+            {
+                if (listView1.Items[X].Checked) {
+                    oXLSheet.Range["A:F"].HorizontalAlignment = XlHAlign.xlHAlignLeft;
+                    oXLSheet.Range[$"A{X + 2}: F{X + 2}"].Value = new object[]
+                    {
+                    listView1.Items[X].Text,
+                    listView1.Items[X].SubItems[1].Text,
+                    listView1.Items[X].SubItems[2].Text,
+                    listView1.Items[X].SubItems[3].Text,
+                    listView1.Items[X].SubItems[4].Text,
+                    listView1.Items[X].SubItems[5].Text,
+                    };
+                }
+            }
+            oXLBook.SaveAs(Filename: $"{Environment.CurrentDirectory}\\Inventaire_{DateTime.Now.ToShortTimeString()}.xlsx");
+            oXLBook.Close();
+            oXLApp.Application.Quit();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            for (int a=0; a<listView1.Items.Count; a++) { listView1.Items[a].Checked = true; }
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            for (int a = 0; a < listView1.Items.Count; a++) { listView1.Items[a].Checked = false; }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = Clipboard.GetText();
+            button1_Click(sender, e);
+        }
+    }
+}
